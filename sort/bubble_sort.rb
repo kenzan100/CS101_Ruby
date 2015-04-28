4a03a42bff7490a412da388d4e8854fd1c88ef71
Yuta Okazaki <s04155yo@gmail.com>
2015-04-28 12:22:02 -0400

    bubble_sort imperatively added

--- sort/bubble_sort.rb
+++ sort/bubble_sort.rb
@@ -1,3 +1,5 @@
+require 'benchmark'
+
 def bubble_sort(arr)
   return [] if arr.empty?
   arr.each_with_index do |elem, i|
@@ -11,4 +13,37 @@ def bubble_sort(arr)
   bubble_sort(arr[0..-2]) + [arr.last]
 end
 
-p bubble_sort([3,2,1,4,10,0,-10])
+def bubble_sort_imp(arr)
+  (arr.length).times do |i|
+    (arr.length).times do |j|
+      print 'c '
+      break unless arr[j+1]
+      if arr[j] > arr[j+1]
+        tmp = arr[j]
+        arr[j] = arr[j+1]
+        arr[j+1] = tmp
+      end
+    end
+  end
+  arr
+end
+
+time = Benchmark.measure do
+  p bubble_sort_imp([3,2,1])
+  puts
+  p bubble_sort_imp([4,3,2,1])
+  puts
+  p bubble_sort_imp([5,4,3,2,1])
+  puts
+  p bubble_sort_imp([6,5,4,3,2,1])
+end
+
+# why bubble_sort O(n^2)?
+# becuase each time you add an element to the list,
+# you're comparing it to every other element of that list.
+# and other element has to compare them to a newly added element as well.
+
+# as length grows by 1,
+# calc grows by length.
+
+puts time.real
