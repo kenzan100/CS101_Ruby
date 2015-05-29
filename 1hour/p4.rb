df5cb44970c801f5ab1fd4e89996909c8e0f0ba8
Yuta Okazaki <s04155yo@gmail.com>
2015-05-28 20:14:38 -0400

    fix to handle edge case

--- 1hour/p4.rb
+++ 1hour/p4.rb
@@ -1,29 +1,23 @@
 require 'pp'
 # my strategy
-# sort by first char of number element
-# if first char is the same number,
-# it stores nth number until it gets the last letter
+# sort by (n+1)th char of number element,
+# as long as nth char is the same number.
 
 def lpn(arr)
   arr.sort do |a,b|
     digit_calc = lambda{|n,i| n.to_s[i].to_i}
     index = 0
-    a_val = digit_calc.call(a,index)
-    b_val = digit_calc.call(b,index)
-    if a_val == b_val
-      if a.to_s.length == b.to_s.length
-        a <=> b
-      else
-        -1 * (a.to_s.length <=> b.to_s.length)
-      end
-    else
-      a_val <=> b_val
-    end
+    begin
+      a_val = digit_calc.call(a,index)
+      b_val = digit_calc.call(b,index)
+      index += 1
+    end while a_val == b_val && (a.to_s[index] || b.to_s[index])
+    a_val <=> b_val
   end.reverse.reduce("") do |acc, n|
     acc += n.to_s
     acc
   end.to_i
 end
 
-arr = [50, 2, 500, 101, 100, 9]
+arr = [2, 221, 25, 9, 1, 10]
 pp lpn(arr)
